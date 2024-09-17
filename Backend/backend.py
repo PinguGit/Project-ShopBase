c730ff8e1363eecbf45d1ba3cc831853d48b7ca2
Hom Tente <126233634+homtente@users.noreply.github.com>
2024-09-17 11:32:09 +0200

    update

--- Backend/backend.py
+++ Backend/backend.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import mysql.connector
+import numpy as np 
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
@@ -11,4 +12,37 @@ def db_connect():
         database="shopsystem"
     )
 
-db_connect()
+def get_produkte():
+    conn = db_connect()
+    cursor = conn.cursor(dictionary=True)
+    query = "SELECT * FROM Produkte"
+
+    cursor.execute(query)
+
+    result = cursor.fetchall()
+    return result
+
+
+products = get_produkte()
+
+
+
+class product:
+    def __init__(self, product_id, product_name, preis, hersteller_id):
+        self.product_id = product_id
+        self.product_name = product_name
+        self.preis = preis
+        self.hersteller_id = hersteller_id
+    def showobject(self):
+        return f"{self.product_id}, {self.product_name}"
+
+
+product_objects = {}
+for n in products:
+    obj = product(n['produkt_id'], n['produkt_name'], n['preis'], n['hersteller_id'])    
+    product_objects[n['produkt_name']] = obj
+
+print(product_objects)
+
+if __name__ == '__main__':
+    app.run(debug=True)
