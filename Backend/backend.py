25b586c08d079f237a34ec9641169cea9ec1ce59
Hom Tente <126233634+homtente@users.noreply.github.com>
2024-09-17 11:22:27 +0200

    newupdate

--- Backend/backend.py
+++ Backend/backend.py
@@ -1,2 +1,14 @@
-print('GAYDRIAN')
-print('Ur Mom')
+from flask import Flask
+import mysql.connector
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+def db_connect():
+    return mysql.connector.connect(        
+        host="172.16.182.142",
+        user="power_user",
+        password="adrian_stinkt",
+        database="shopsystem"
+    )
+
+db_connect()
