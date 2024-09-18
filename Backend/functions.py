aecdeb68ee75ccfcf72fa884bcd89e77c78915bd
Hom Tente <126233634+homtente@users.noreply.github.com>
2024-09-18 13:08:31 +0200

    function edit product und update product

--- Backend/functions.py
+++ Backend/functions.py
@@ -10,10 +10,38 @@ def db_connect():
         database="shopsystem"
     )
 
-def create_product(product_name, ):
+def create_product(product_name, price):
     conn = db_connect()
     cursor = conn.cursor(dictionary=True)
-    query = "INSERT INTO produkte table ('produkt_name', 'preis') VALUES '{}')"
+    query = "INSERT INTO produkte table ('produkt_name', 'preis') VALUES (%s, %s))"
+    cursor.execute(query, (product_name, price))
+    result = cursor.fetchall
+    conn.close
+    return result
+
+def edit_product(product_id, product_name = None, price = None):
+    conn = db_connect()
+    cursor = conn.cursor(dictionary=True)
+
+    fields = []
+    values = []
+    if product_name:
+        fields.append("produkt_name= %s")
+        values.append(product_name)
+
+    if price is not None:
+        fields.append("price = %s")
+        values.append(price)
+
+    if not fields:
+        print("Keine Änderungen")
+        return
+    
+    query = "UPDATE produkte SET " + ", " .join(fields) + "WHERE produkt_id = %s"
+    values.append(product_id)
+    cursor.execute(query, values)
+    result = cursor.fetchall()
+    return result
 
 
 def get_produkte():
