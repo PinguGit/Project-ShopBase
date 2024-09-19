d422a807d778be59120757204dfb6650e811d80a
Whopper-Plantbased <tizianbachmann24@gmail.com>
2024-09-19 09:44:50 +0200

    Added Repository Classes

--- Backend/classes.py
+++ Backend/classes.py
@@ -22,3 +22,46 @@ class country:
         self.country = country
     def showobject(self):
         return f"{self.country_id}, {self.country}"
+    
+class password:
+    def __init__(self, password_id, password):
+        self.password_id = password_id
+        self.password = password
+    def showobject(self):
+        return f"{self.password_id}, {self.password}"
+    
+class location:
+    def __init__(self, location_id, zip, location):
+        self.location_id = location_id
+        self.zip = zip
+        self.location = location
+    def showobject(self):
+        return f"{self.location_id}, {self.zip}, {self.location}"
+    
+class Vendor:
+    def __init__(self, vendor_id, name, street, houseNumber, email, ort_id, laender_id, password_id):
+        self.vendor_id = vendor_id
+        self.name = name
+        self.street = street
+        self.houseNumber = houseNumber
+        self.email = email
+        self.ort_id = ort_id
+        self.laender_id = laender_id
+        self.password_id = password_id
+
+    def showObject(self):
+        return f"{self.vendor_id}, {self.name}, {self.street}, {self.houseNumber}, {self.email}, {self.ort_id}, {self.laender_id}, {self.password_id}"
+
+
+class Customer(Vendor):
+    def __init__(self, vendor_id, name, street, houseNumber, email, ort_id, laender_id, password_id, forename, birthday):
+        super().__init__(vendor_id, name, street, houseNumber, email, ort_id, laender_id, password_id)
+        self.customer_id = vendor_id  # kunde_id ist nur ein Alias für vendor_id
+        self.forename = forename
+        self.birthday = birthday
+
+    def showObject(self):
+        return f"{self.customer_id}, {self.forename}, {self.birthday}, {self.name}, {self.street}, {self.houseNumber}, {self.email}, {self.ort_id}, {self.laender_id}, {self.password_id}"
+
+
+    
