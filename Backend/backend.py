20d7da388189ce68d6879f68e02bd8dc5d7b0bbc
Hom Tente <126233634+homtente@users.noreply.github.com>
2024-09-20 10:55:53 +0200

    Update

--- Backend/backend.py
+++ Backend/backend.py
@@ -2,13 +2,8 @@ from flask import Flask
 import mysql.connector
 import classes
 import functions
-<<<<<<< HEAD
 import db_get
 import db_delete
-=======
-import db_delete
-
->>>>>>> 94db405 (Update Backend.py)
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
@@ -21,7 +16,7 @@ for n in products:
     obj_products = classes.product(n['produkt_id'], n['produkt_name'], n['preis'], n['hersteller_id'])    
     product_objects[n['produkt_id']] = obj_products
 
-manufactures = functions.get_manufacturer
+manufactures = db_get.get_manufacturer
 manufactures_objects_dic = {}
 
 for n in manufactures_objects_dic:
