f508bae0d91957c8c509fb3b7335184306425b41
Hom Tente <126233634+homtente@users.noreply.github.com>
2024-09-20 10:28:39 +0200

    Update Backend.py

--- Backend/backend.py
+++ Backend/backend.py
@@ -2,8 +2,13 @@ from flask import Flask
 import mysql.connector
 import classes
 import functions
+<<<<<<< HEAD
 import db_get
 import db_delete
+=======
+import db_delete
+
+>>>>>>> 94db405 (Update Backend.py)
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
@@ -13,12 +18,23 @@ products = db_get.get_product()
 product_objects = {}
 
 for n in products:
-    obj = classes.product(n['produkt_id'], n['produkt_name'], n['preis'], n['hersteller_id'])    
-    product_objects[n['produkt_name']] = obj
+    obj_products = classes.product(n['produkt_id'], n['produkt_name'], n['preis'], n['hersteller_id'])    
+    product_objects[n['produkt_id']] = obj_products
+
+manufactures = functions.get_manufacturer
+manufactures_objects_dic = {}
+
+for n in manufactures_objects_dic:
+    obj_manufactures = classes.manufacturer(n["hersteller_id"], n["hersteller_name"], n["laender_id"])
+    manufactures_objects_dic[n]
 
 print(product_objects)
 
-print(functions.delete_by_obejct(product_objects.get("Wasser")))
+
+
+
+
+print(db_delete.delete_by_obejct(product_objects.get("Wasser")))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Backend/functions.py
+++ Backend/functions.py
@@ -3,16 +3,60 @@ import mysql.connector
 
 def db_connect():
     return mysql.connector.connect(        
-        host="172.16.182.142",
+        host="172.16.182.145",
         user="power_user",
         password="adrian_stinkt",
         database="shopsystem"
     )
 
+<<<<<<< HEAD
 def delete_by_obejct(obj):
     return(type(obj).__name__)
 
 
+=======
+def create_product(product_name, price):
+    conn = db_connect()
+    cursor = conn.cursor(dictionary=True)
+    query = "INSERT INTO produkte table ('produkt_name', 'preis') VALUES (%s, %s))"
+    cursor.execute(query, (product_name, price))
+    result = cursor.fetchall
+    conn.close
+    return result
+
+
+#Function to edit a product using the id, possible to change the name and price
+def edit_product(product_id, product_name = None, price = None):
+    if product_name is not None or isinstance(product_name, str):
+        raise ValueError("Product name must be a string!")
+    conn = db_connect()
+    cursor = conn.cursor(dictionary=True)
+    #dynamic array to only change given arguments
+    fields = []
+    values = []
+
+    #add %s to fields
+    if product_name:
+        fields.append("produkt_name= %s")
+        values.append(product_name)
+
+    if price is not None:
+        fields.append("price = %s")
+        values.append(price)
+
+    if not fields:
+        print("Keine Änderungen")
+        return
+    
+    query = "UPDATE product SET " + ", " .join(fields) + " WHERE produkt_id = %s"
+    
+    #add product id to use
+    values.append(product_id)
+    cursor.execute(query, values)
+    conn.commit() 
+    return(f"Changed {product_id}")
+
+>>>>>>> 94db405 (Update Backend.py)
 
 def delete():
     ""
