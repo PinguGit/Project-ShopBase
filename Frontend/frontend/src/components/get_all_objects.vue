da3cf433020329cf18d4b244c94ed83a76b58efc
honmtente <hente.tom@gmail.com>
2024-10-07 14:04:19 +0200

    vuejs

---
+++ Frontend/frontend/src/components/get_all_objects.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="get_all_objects">
+      <h1>Objekte</h1>
+      <p v-bind=fetchallobjects()></p>
+      <ul>
+        <li v-for="(object, index) in objects" :key="index">{{ object.name }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'get_all_objects',
+    data() {
+      return {
+        objects: [], // Array, um die abgerufenen Objekte zu speichern
+        tableName: 'products', // Beispiel-Tabellenname, kann dynamisch geändert werden
+        
+    
+    };
+    },
+    mounted() {
+      this.fetchallobjects(this.tableName); // Beim Laden der Komponente werden die Objekte abgerufen
+    },
+    methods: {
+      fetchallobjects(tablename) {
+        fetch(`/api/get_all_objects/${tablename}`)
+          .then(response => response.json()) // Konvertiere die Antwort in JSON
+          .then(data => {
+            this.objects = data; // Speichere die abgerufenen Objekte im Array
+          })
+          .catch(error => {
+            console.error("Fehler beim Abrufen der Objekte:", error);
+          });
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Dein CSS hier */
+  </style>
