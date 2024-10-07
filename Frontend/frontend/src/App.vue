92392510f366ec0478e50e6b980aa4f931682e2e
Hom Tente <126233634+homtente@users.noreply.github.com>
2024-10-07 10:23:09 +0200

    vuejs api get

--- Frontend/frontend/src/App.vue
+++ Frontend/frontend/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <get_all_objects />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import get_all_objects from './components/get_all_objects.vue';
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    get_all_objects,
   }
 }
 </script>
