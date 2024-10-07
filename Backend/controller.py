32c6ebbf2c6f9e6283bbc58c111394426f444745
Whopper-Plantbased <tizianbachmann24@gmail.com>
2024-10-07 13:45:14 +0200

    added GetOrders Api

--- Backend/controller.py
+++ Backend/controller.py
@@ -1,4 +1,4 @@
-from Backend.db_get import get_all_objects, get_object_by_id
+import db_get
 from flask import Flask, jsonify
 import getCommand
 from flask_cors import CORS
@@ -9,13 +9,18 @@ CORS(app)
 
 @app.route('/api/get_object/<table>/<objectId>', methods=['GET'])
 def getObject(objectId, table):
-    dictionary = get_object_by_id(table, objectId)
+    dictionary = db_get.get_object_by_id(table, objectId)
     object = getType(table, dictionary, 0)
     return jsonify(object)
 
+@app.route('/api/get_orders/<customerId>', methods=['GET'])
+def getOrdersById(customerId):
+    dictionary = db_get.get_customer_orders(customerId)
+    return jsonify(dictionary)
+
 @app.route('/api/get_all_objects/<table>', methods=['GET'])
 def getAllObjects(table):
-    list = get_all_objects(table)
+    list = db_get.get_all_objects(table)
     objects = getType(table, list, 1)
     return jsonify(objects)
 
