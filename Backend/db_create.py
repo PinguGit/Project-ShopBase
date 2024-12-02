298bb75f3b2b8bb3ff4be291fcb3bf7aa4419fb4
Whopper-Plantbased <tizianbachmann24@gmail.com>
2024-12-02 11:36:47 +0100

    bla

--- Backend/db_create.py
+++ Backend/db_create.py
@@ -127,10 +127,10 @@ def login_user(email, entered_password, isCustomer):
 
     # verify password
     if bcrypt.checkpw(entered_password.encode('utf-8'), stored_hashed_password.encode('utf-8')):
-        if isCustomer == 'private' and 'kunden_id' in result:
+        if isCustomer == 'private' in result:
             print("Login successful as customer.")
             return result  # Return the customer data
-        elif isCustomer == 'business' and 'verkaeufer_id' in result:
+        elif isCustomer == 'business' in result:
             print("Login successful as vendor.")
             return result  # Return the vendor data
         else:
